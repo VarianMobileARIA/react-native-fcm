4f88ab2981d622989ebf52eabe8259023d3f694d
Sushant Sardeshpande <ssardeshpande@elivotech.com>
2018-01-02 16:52:17 +0530

    - Local notifications hidden if network connected

--- android/src/main/java/com/evollu/react/fcm/SendNotificationTask.java
+++ android/src/main/java/com/evollu/react/fcm/SendNotificationTask.java
@@ -11,6 +11,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.RingtoneManager;
+import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -40,6 +41,11 @@ public class SendNotificationTask extends AsyncTask<Void, Void, Void> {
         this.sharedPreferences = sharedPreferences;
         this.mIsForeground = mIsForeground;
     }
+
+    public boolean isNetworkAvailable(Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
     
     protected Void doInBackground(Void... params) {
         try {
@@ -60,162 +66,164 @@ public class SendNotificationTask extends AsyncTask<Void, Void, Void> {
                 ApplicationInfo appInfo = mContext.getApplicationInfo();
                 title = mContext.getPackageManager().getApplicationLabel(appInfo).toString();
             }
-            
-            NotificationCompat.Builder notification = new NotificationCompat.Builder(mContext)
-            .setContentTitle(title)
-            .setContentText(bundle.getString("body"))
-            .setTicker(bundle.getString("ticker"))
-            .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
-            .setAutoCancel(bundle.getBoolean("auto_cancel", true))
-            .setNumber((int)bundle.getDouble("number"))
-            .setSubText(bundle.getString("sub_text"))
-            .setGroup(bundle.getString("group"))
-            .setVibrate(new long[]{0, DEFAULT_VIBRATION})
-            .setExtras(bundle.getBundle("data"));
-            
-            if (bundle.containsKey("ongoing") && bundle.getBoolean("ongoing")) {
-                notification.setOngoing(bundle.getBoolean("ongoing"));
-            }
-            
-            //priority
-            String priority = bundle.getString("priority", "");
-            switch(priority) {
-                case "min":
-                    notification.setPriority(NotificationCompat.PRIORITY_MIN);
-                    break;
-                case "high":
-                    notification.setPriority(NotificationCompat.PRIORITY_HIGH);
-                    break;
-                case "max":
-                    notification.setPriority(NotificationCompat.PRIORITY_MAX);
-                    break;
-                default:
-                    notification.setPriority(NotificationCompat.PRIORITY_DEFAULT);
-            }
-            
-            //icon
-            String smallIcon = bundle.getString("icon", "ic_launcher");
-            int smallIconResId = res.getIdentifier(smallIcon, "mipmap", packageName);
-            if(smallIconResId == 0){
-                smallIconResId = res.getIdentifier(smallIcon, "drawable", packageName);
-            }
-            if(smallIconResId != 0){
-                notification.setSmallIcon(smallIconResId);
-            }
-            
-            //large icon
-            String largeIcon = bundle.getString("large_icon");
-            if(largeIcon != null && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP){
-                if (largeIcon.startsWith("http://") || largeIcon.startsWith("https://")) {
-                    Bitmap bitmap = getBitmapFromURL(largeIcon);
-                    notification.setLargeIcon(bitmap);
-                } else {
-                    int largeIconResId = res.getIdentifier(largeIcon, "mipmap", packageName);
-                    Bitmap largeIconBitmap = BitmapFactory.decodeResource(res, largeIconResId);
-                    
-                    if (largeIconResId != 0) {
-                        notification.setLargeIcon(largeIconBitmap);
+
+            if (bundle.getString("isRemote") != null  || (bundle.getString("isRemote") == null && !isNetworkAvailable(this.mContext))) {
+                NotificationCompat.Builder notification = new NotificationCompat.Builder(mContext)
+                        .setContentTitle(title)
+                        .setContentText(bundle.getString("body"))
+                        .setTicker(bundle.getString("ticker"))
+                        .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
+                        .setAutoCancel(bundle.getBoolean("auto_cancel", true))
+                        .setNumber((int)bundle.getDouble("number"))
+                        .setSubText(bundle.getString("sub_text"))
+                        .setGroup(bundle.getString("group"))
+                        .setVibrate(new long[]{0, DEFAULT_VIBRATION})
+                        .setExtras(bundle.getBundle("data"));
+
+                if (bundle.containsKey("ongoing") && bundle.getBoolean("ongoing")) {
+                    notification.setOngoing(bundle.getBoolean("ongoing"));
+                }
+
+                //priority
+                String priority = bundle.getString("priority", "");
+                switch(priority) {
+                    case "min":
+                        notification.setPriority(NotificationCompat.PRIORITY_MIN);
+                        break;
+                    case "high":
+                        notification.setPriority(NotificationCompat.PRIORITY_HIGH);
+                        break;
+                    case "max":
+                        notification.setPriority(NotificationCompat.PRIORITY_MAX);
+                        break;
+                    default:
+                        notification.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                }
+
+                //icon
+                String smallIcon = bundle.getString("icon", "ic_launcher");
+                int smallIconResId = res.getIdentifier(smallIcon, "mipmap", packageName);
+                if(smallIconResId == 0){
+                    smallIconResId = res.getIdentifier(smallIcon, "drawable", packageName);
+                }
+                if(smallIconResId != 0){
+                    notification.setSmallIcon(smallIconResId);
+                }
+
+                //large icon
+                String largeIcon = bundle.getString("large_icon");
+                if(largeIcon != null && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP){
+                    if (largeIcon.startsWith("http://") || largeIcon.startsWith("https://")) {
+                        Bitmap bitmap = getBitmapFromURL(largeIcon);
+                        notification.setLargeIcon(bitmap);
+                    } else {
+                        int largeIconResId = res.getIdentifier(largeIcon, "mipmap", packageName);
+                        Bitmap largeIconBitmap = BitmapFactory.decodeResource(res, largeIconResId);
+
+                        if (largeIconResId != 0) {
+                            notification.setLargeIcon(largeIconBitmap);
+                        }
                     }
                 }
-            }
-            
-            //big text
-            String bigText = bundle.getString("big_text");
-            if(bigText != null){
-                notification.setStyle(new NotificationCompat.BigTextStyle().bigText(bigText));
-            }
-            
-            //picture
-            String picture = bundle.getString("picture");
-            if(picture!=null){
-                NotificationCompat.BigPictureStyle bigPicture = new NotificationCompat.BigPictureStyle();
-                
-                if (picture.startsWith("http://") || picture.startsWith("https://")) {
-                    Bitmap bitmap = getBitmapFromURL(picture);
-                    bigPicture.bigPicture(bitmap);
-                } else {
-                    int pictureResId = res.getIdentifier(picture, "mipmap", packageName);
-                    Bitmap pictureResIdBitmap = BitmapFactory.decodeResource(res, pictureResId);
-                    
-                    if (pictureResId != 0) {
-                        bigPicture.bigPicture(pictureResIdBitmap);
+
+                //big text
+                String bigText = bundle.getString("big_text");
+                if(bigText != null){
+                    notification.setStyle(new NotificationCompat.BigTextStyle().bigText(bigText));
+                }
+
+                //picture
+                String picture = bundle.getString("picture");
+                if(picture!=null){
+                    NotificationCompat.BigPictureStyle bigPicture = new NotificationCompat.BigPictureStyle();
+
+                    if (picture.startsWith("http://") || picture.startsWith("https://")) {
+                        Bitmap bitmap = getBitmapFromURL(picture);
+                        bigPicture.bigPicture(bitmap);
+                    } else {
+                        int pictureResId = res.getIdentifier(picture, "mipmap", packageName);
+                        Bitmap pictureResIdBitmap = BitmapFactory.decodeResource(res, pictureResId);
+
+                        if (pictureResId != 0) {
+                            bigPicture.bigPicture(pictureResIdBitmap);
+                        }
+                    }
+                    bigPicture.setBigContentTitle(title);
+                    bigPicture.setSummaryText(bundle.getString("body"));
+
+                    notification.setStyle(bigPicture);
+                }
+
+                //sound
+                String soundName = bundle.getString("sound");
+                if (soundName != null) {
+                    if (soundName.equalsIgnoreCase("default")) {
+                        notification.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+                    } else {
+                        int soundResourceId = res.getIdentifier(soundName, "raw", packageName);
+                        if (soundResourceId == 0) {
+                            soundName = soundName.substring(0, soundName.lastIndexOf('.'));
+                            soundResourceId = res.getIdentifier(soundName, "raw", packageName);
+                        }
+                        notification.setSound(Uri.parse("android.resource://" + packageName + "/" + soundResourceId));
                     }
                 }
-                bigPicture.setBigContentTitle(title);
-                bigPicture.setSummaryText(bundle.getString("body"));
-                
-                notification.setStyle(bigPicture);
-            }
-            
-            //sound
-            String soundName = bundle.getString("sound");
-            if (soundName != null) {
-                if (soundName.equalsIgnoreCase("default")) {
-                    notification.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
-                } else {
-                    int soundResourceId = res.getIdentifier(soundName, "raw", packageName);
-                    if (soundResourceId == 0) {
-                        soundName = soundName.substring(0, soundName.lastIndexOf('.'));
-                        soundResourceId = res.getIdentifier(soundName, "raw", packageName);
+
+                //color
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    notification.setCategory(NotificationCompat.CATEGORY_CALL);
+
+                    String color = bundle.getString("color");
+                    if (color != null) {
+                        notification.setColor(Color.parseColor(color));
                     }
-                    notification.setSound(Uri.parse("android.resource://" + packageName + "/" + soundResourceId));
                 }
-            }
-            
-            //color
-            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                notification.setCategory(NotificationCompat.CATEGORY_CALL);
-                
-                String color = bundle.getString("color");
-                if (color != null) {
-                    notification.setColor(Color.parseColor(color));
+
+                //vibrate
+                if(bundle.containsKey("vibrate")){
+                    long vibrate = Math.round(bundle.getDouble("vibrate", DEFAULT_VIBRATION));
+                    if(vibrate > 0){
+                        notification.setVibrate(new long[]{0, vibrate});
+                    }else{
+                        notification.setVibrate(null);
+                    }
                 }
-            }
-            
-            //vibrate
-            if(bundle.containsKey("vibrate")){
-                long vibrate = Math.round(bundle.getDouble("vibrate", DEFAULT_VIBRATION));
-                if(vibrate > 0){
-                    notification.setVibrate(new long[]{0, vibrate});
-                }else{
-                    notification.setVibrate(null);
+
+                //lights
+                if (bundle.getBoolean("lights")) {
+                    notification.setDefaults(NotificationCompat.DEFAULT_LIGHTS);
                 }
-            }
-            
-            //lights
-            if (bundle.getBoolean("lights")) {
-                notification.setDefaults(NotificationCompat.DEFAULT_LIGHTS);
-            }
-            
-            if(bundle.containsKey("fire_date")) {
-                Log.d(TAG, "broadcast intent if it is a scheduled notification");
-                Intent i = new Intent("com.evollu.react.fcm.ReceiveLocalNotification");
-                i.putExtras(bundle);
-                mContext.sendOrderedBroadcast(i, null);
-            }
-            
-            if(!mIsForeground || bundle.getBoolean("show_in_foreground")){
-                Intent intent = new Intent();
-                intent.setClassName(mContext, intentClassName);
-                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                intent.putExtras(bundle);
-                intent.setAction(bundle.getString("click_action"));
-                
-                int notificationID = bundle.containsKey("id") ? bundle.getString("id", "").hashCode() : (int) System.currentTimeMillis();
-                PendingIntent pendingIntent = PendingIntent.getActivity(mContext, notificationID, intent,
-                                                                        PendingIntent.FLAG_UPDATE_CURRENT);
-                
-                notification.setContentIntent(pendingIntent);
-                
-                Notification info = notification.build();
-                
-                NotificationManagerCompat.from(mContext).notify(notificationID, info);
-            }
-            //clear out one time scheduled notification once fired
-            if(!bundle.containsKey("repeat_interval") && bundle.containsKey("fire_date")) {
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.remove(bundle.getString("id"));
-                editor.apply();
+
+                if(bundle.containsKey("fire_date")) {
+                    Log.d(TAG, "broadcast intent if it is a scheduled notification");
+                    Intent i = new Intent("com.evollu.react.fcm.ReceiveLocalNotification");
+                    i.putExtras(bundle);
+                    mContext.sendOrderedBroadcast(i, null);
+                }
+
+                if(!mIsForeground || bundle.getBoolean("show_in_foreground")){
+                    Intent intent = new Intent();
+                    intent.setClassName(mContext, intentClassName);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                    intent.putExtras(bundle);
+                    intent.setAction(bundle.getString("click_action"));
+
+                    int notificationID = bundle.containsKey("id") ? bundle.getString("id", "").hashCode() : (int) System.currentTimeMillis();
+                    PendingIntent pendingIntent = PendingIntent.getActivity(mContext, notificationID, intent,
+                            PendingIntent.FLAG_UPDATE_CURRENT);
+
+                    notification.setContentIntent(pendingIntent);
+
+                    Notification info = notification.build();
+
+                    NotificationManagerCompat.from(mContext).notify(notificationID, info);
+                }
+                //clear out one time scheduled notification once fired
+                if(!bundle.containsKey("repeat_interval") && bundle.containsKey("fire_date")) {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.remove(bundle.getString("id"));
+                    editor.apply();
+                }
             }
         } catch (Exception e) {
             Log.e(TAG, "failed to send local notification", e);
@@ -244,4 +252,3 @@ public class SendNotificationTask extends AsyncTask<Void, Void, Void> {
         return className;
     }
 }
-
